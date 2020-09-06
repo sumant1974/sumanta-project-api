234e40818d6e8826e8ffa8713476d75675572b36
Sumanta Sahoo <sumant1974@gmail.com>
2020-09-07 00:34:23 +0530

    authorized admin to get partner dashboard

--- api/partners/getdashboard.php
+++ api/partners/getdashboard.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-$auth_role=array("1","2","3");
+$auth_role=array("1");
 // files for decoding jwt will be here
 // required to encode json web token
 include_once '../config/core.php';
